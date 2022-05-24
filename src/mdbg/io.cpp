457d7f87275e2017ee1cc4b92572d95be3a4946c
yatsukha <yatsukha.roman@gmail.com>
2022-05-24 19:46:29 +0000

    Add fastq/fq support.

--- src/mdbg/io.cpp
+++ src/mdbg/io.cpp
@@ -47,11 +47,33 @@ namespace fast {
 
 namespace mdbg {
 
+  ::std::string extension(::std::string const& filename) noexcept {
+    auto const base = ::std::filesystem::path{filename}.filename().string();
+    auto const period = base.find('.');
+
+    if (period == ::std::string::npos) {
+      return "";
+    }
+
+    return base.substr(period);
+  }
+
+  bool fastq(::std::string const& extension) noexcept {
+    return
+      extension.find(".fastq") != ::std::string::npos ||
+      extension.find(".fq") != ::std::string::npos;
+  }
+
   ::std::vector<::std::unique_ptr<::std::string>> 
   load_sequences(::std::string const& input) noexcept {
     if (!::std::filesystem::exists(input)) {
       ::mdbg::terminate("Could not locate given file: ", input);
     }
+
+    if (fastq(extension(input))) {
+      return ::fast::CreateFastqParser<::std::string>(input.c_str()).Parse();
+    }
+
     return ::fast::CreateFastaParser<::std::string>(input.c_str()).Parse();
   }
 
