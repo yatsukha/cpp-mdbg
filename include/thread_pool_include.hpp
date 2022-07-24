d53c5464f8b1cb909d624ad7c4e6d59a19102298
yatsukha <yatsukha.roman@gmail.com>
2022-07-24 20:10:57 +0000

    Minor adjustments.

--- include/thread_pool_include.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wfloat-conversion"
-#pragma GCC diagnostic ignored "-Wparentheses"
-#include <thread_pool.hpp>
-#pragma GCC diagnostic pop
