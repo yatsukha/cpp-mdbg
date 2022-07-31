672528aa37830aca9bdfb23262345b9a1db0a9b6
yatsukha <yatsukha.roman@gmail.com>
2022-07-31 18:02:38 +0000

    Remove unused file.

--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <mdbg/io/parser.hpp>
-
-int main(int const, char const* const* argv) {
-  ::mdbg::io::fasta_constumer lambda = [](auto&& name, auto&& seq) {
-    ::std::cout << ">" << name << "\n" << seq << "\n";
-  };
-  ::mdbg::io::parse_fasta(argv[1], lambda);
-}
